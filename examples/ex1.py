107fd993a0db6a531da53522b59db7e9933ae5c6
rxiao <ben.xiao@me.com>
2020-11-24 21:17:18 +1100

    add comments

--- examples/ex1.py
+++ examples/ex1.py
@@ -12,22 +12,27 @@ def oracle(labels, idx):
 
 
 if __name__ == '__main__':
+    # from neil's example dataset
     data = json.load(open('data.json'))
     labels = np.array(data['labels'])
     scores = np.array(data['scores'])
     preds = np.array(data['preds'])
     probs = scores2probs(scores)
 
+    # initialize all samplers
     oracle = partial(oracle, labels)
     passive_sampler = PassiveSampler(0.5, preds, probs, oracle)
     importance_sampler = ImportanceSampler(0.5, preds, probs, oracle)
     druck_sampler = DruckSampler(0.5, preds, probs, oracle)
 
+    # sample
     passive_sampler.sample_distinct(5000)
     importance_sampler.sample_distinct(5000)
     druck_sampler.sample_distinct(5000)
 
     # its likely that f score from passive sampler is all NAN, consider increase number of samples
+
+    # f score estimates
     print(f"{passive_sampler.f_score_history()=}")
     print(f"{importance_sampler.f_score_history()=}")
     print(f"{druck_sampler.f_score_history()=}")
