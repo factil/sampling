6e70fc7476fe7a55ec0a05a772ae3fbcec72c1e0
benxiao <ben.xiao@me.com>
2020-11-30 17:51:57 +1100

    add my sampler

--- examples/ex2.py
+++ examples/ex2.py
@@ -3,8 +3,10 @@ from functools import partial
 import numpy as np
 import tqdm
 from oasis import OASISSampler
-
+from mine import MySampler
 from utility import scores2probs
+
+OASISSampler.f_score_history = lambda x: x.estimate_
 
 
 def oracle(labels, idx):
@@ -21,14 +23,18 @@ if __name__ == '__main__':
 
     # initialize all samplers
     oracle = partial(oracle, labels)
-    n_runs = 50
+    n_runs = 30
     f_scores = np.empty(n_runs)
-    for i in tqdm.tqdm(range(n_runs)):
-        sampler = OASISSampler(0.5, preds, probs, oracle)
-        sampler.sample_distinct(5000)
-        f_scores[i] = sampler.estimate_[-1]
-    print("estimated f score")
-    print(f"{np.mean(f_scores)=}")
-    print(f"{np.std(f_scores)=}")
-    print(f"{f_scores.max()=}")
-    print(f"{f_scores.min()=}")
+    for cls in [OASISSampler, MySampler]:
+        print(cls.__name__)
+        for i in tqdm.tqdm(range(n_runs)):
+            sampler = cls(0.5, preds, probs, oracle)
+            sampler.sample_distinct(5000)
+            f_scores[i] = sampler.f_score_history()[-1]
+        print("estimated f score")
+        print(f"{list(f_scores)=}")
+        print(f"{np.mean(f_scores)=}")
+        print(f"{np.std(f_scores)=}")
+        print(f"{f_scores.max()=}")
+        print(f"{f_scores.min()=}")
+        print(f"{'*' + '-' * 30 + '*'}")
---
+++ mine.py
@@ -0,0 +1,40 @@
+from typing import Callable
+from collections import Counter
+import random
+import numpy as np
+from sampler_abc import BaseSampler
+from utility import compute_f_score
+
+
+class MySampler(BaseSampler):
+    def __init__(self, alpha: float,
+                 predictions,  # 1d numpy array
+                 scores,  # 1d numpy array
+                 oracle: Callable,
+                 n_bins=30,
+                 max_iter=None):
+        super().__init__(alpha, predictions, scores, oracle, max_iter=max_iter)
+        self.TP, self.FP, self.FN = [0] * 3
+        bins = [x/n_bins for x in range(n_bins+1)]
+        # print(bins)
+        self.strata_allocations = np.digitize(self.scores, bins, right=True)
+        strata_counts = Counter(self.strata_allocations)
+        self.n_totals = len(scores)
+        strata_proportions = {k: v/self.n_totals for k, v in strata_counts.items()}
+        self.weights = {k: v/(1/n_bins) for k, v in strata_proportions.items()}
+
+    def select_next_item(self, sample_with_replacement: bool, **kwargs):
+        strata_idx = random.choice(list(self.weights.keys()))
+        loc = np.random.choice(np.arange(self.n_totals)[self.strata_allocations == strata_idx])
+        return loc, self.weights[strata_idx], None
+
+    def update_estimate_and_sampler(self, ell, ell_hat, weight, **kwargs):
+        """Update the estimate after querying the label for an item"""
+        self.TP += ell_hat * ell * weight
+        self.FP += ell_hat * (1 - ell) * weight
+        self.FN += (1 - ell_hat) * ell * weight
+        self.f_scores[self.idx] = compute_f_score(self.alpha, self.TP, self.FP, self.FN)
+
+
+
+
